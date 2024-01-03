e605b1e859e3bb5c33dcaacfdfd9c385dc67bb9f
root <root@caravan-solar.teganandjosh.com>
2024-01-04 07:55:37 +1000

    added mysql table template

---
+++ victron.sql
@@ -0,0 +1,33 @@
+-- MariaDB dump 10.19  Distrib 10.5.21-MariaDB, for debian-linux-gnueabihf (armv7l)
+--
+-- Host: localhost    Database: victron
+-- ------------------------------------------------------
+-- Server version	10.5.21-MariaDB-0+deb11u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `sensors`
+--
+
+DROP TABLE IF EXISTS `sensors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sensors` (
+  `sensor_name` varchar(255) NOT NULL,
+  `value` varchar(255) NOT NULL,
+  `unit` varchar(255) NOT NULL,
+  UNIQUE KEY `sensor_name` (`sensor_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- Dump completed on 2024-01-04  7:54:41
